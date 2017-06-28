635a64f47f9adf2f9fa7d3d8ce696526fdf671d1
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-06-28 13:50:54 +0900

    DRF3 튜토리얼1-직렬화 한번해봄

--- django_app/snippets/views.py
+++ django_app/snippets/views.py
@@ -1,3 +1,64 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 from django.shortcuts import render
 
-# Create your views here.
+
+class JSONResponse(HttpResponse):
+    """
+    콘텐츠를 JSON으로 변환한 후 HttpResponse 형태로 반환합니다.
+    """
+
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'applications/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    코드 조각을 모두 보여주거나 새 코드 조각을 만듭니다.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=404)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    코드 조각 조회, 업데이트, 삭제
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoseNotExist:
+        return HttpResponse(stats=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parser(request)
+        serializer - SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.data, status=404)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
